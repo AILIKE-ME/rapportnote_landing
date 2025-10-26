43d24bff35431e9332d8b6d7a70a3bc4a17fbdd6
AILIKE-ME <hrpark@ailike.me>
2025-10-26 16:09:52 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // /landing 경로로 배포할 때 필요한 설정
+  basePath: '/landing',
   images: {
     domains: [
       // API 서버 도메인이나 외부 이미지 도메인을 여기에 추가
